a70c4e18043134f85d3b5c8a51a0026750662495
Warlley <warlleymathias1.1@gmail.com>
2026-09-24 07:59:53 -0300

    refactor: ajustes no fluxo do login com spring security, para ser consumido por outro app.

--- src/main/java/com/warlley/anotapedido_api/dto/ProdutoRequestDTO.java
+++ src/main/java/com/warlley/anotapedido_api/dto/ProdutoRequestDTO.java
@@ -1,6 +1,5 @@
 package com.warlley.anotapedido_api.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
--- src/main/java/com/warlley/anotapedido_api/repository/ProdutoRepository.java
+++ src/main/java/com/warlley/anotapedido_api/repository/ProdutoRepository.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto,Long> {
-      boolean existsByNomeFalse(String nome);
 
       boolean existsByNome(@Size(min = 2, message = "nome não pode ser menor que 2 letras.") @NotBlank(message = "campo nome não pode ser vazio.") String nome);
 }
